7917210237c40f105e064e21d07981a983683b5f
Postman67 <43255710+Postman67@users.noreply.github.com>
2025-07-28 20:59:45 -0700

    Created basic cog stuctures

---
+++ cogs/entry_create.py
@@ -0,0 +1,10 @@
+# Cog to handle entry creation
+
+import asyncio
+import os
+import time
+import discord
+from discord import app_commands
+from discord.ext import commands
+from dotenv import load_dotenv
+
